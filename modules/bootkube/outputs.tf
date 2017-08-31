077ff68524654a789f67a99a147ec615609898bc
Richard Lander <rlander7@ford.com>
2017-08-30 23:23:59 -0400

    apicert: Fix ca cert var name

--- modules/bootkube/outputs.tf
+++ modules/bootkube/outputs.tf
@@ -44,11 +44,11 @@ output "systemd_path_unit_rendered" {
   value = "${data.template_file.bootkube_path_unit.rendered}"
 
 output "ca_cert" {
-  value = "${var.existing_certs["ca_cert_path"] == "/dev/null" ? join(" ", tls_self_signed_cert.kube_ca.*.cert_pem) : "${file(var.existing_certs["ca_cert_path"])}${tls_self_signed_cert.kube_ca.0.cert_pem}"}"
+  value = "${var.existing_certs["ca_crt_path"] == "/dev/null" ? join(" ", tls_self_signed_cert.kube_ca.*.cert_pem) : "${file(var.existing_certs["ca_crt_path"])}${tls_self_signed_cert.kube_ca.0.cert_pem}"}"
 }
 
 output "ca_key_alg" {
-  value = "${var.existing_certs["ca_cert_path"] == "/dev/null" ? join(" ", tls_self_signed_cert.kube_ca.*.key_algorithm) : var.existing_certs["ca_key_alg"]}"
+  value = "${var.existing_certs["ca_crt_path"] == "/dev/null" ? join(" ", tls_self_signed_cert.kube_ca.*.key_algorithm) : var.existing_certs["ca_key_alg"]}"
 }
 
 output "ca_key" {
