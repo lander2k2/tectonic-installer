2092072986e9d6f944808687c7cc0bf6f0ecea88
Richard Lander <lander2k2@protonmail.com>
2017-10-19 11:48:44 -0400

    Remove reference to non-existent file

--- modules/vmware/node/ignition.tf
+++ modules/vmware/node/ignition.tf
@@ -12,7 +12,6 @@ data "ignition_config" "node" {
     "${data.ignition_file.profile_node.id}",
     "${data.ignition_file.profile_systemd.id}",
     "${data.ignition_file.nfs_node.id}",
-    "${data.ignition_file.iscsi_node.id}",
     "${data.ignition_file.trusted_ca.id}",
     "${data.ignition_file.ntp_conf.id}",
   ]
