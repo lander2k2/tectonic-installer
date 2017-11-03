b2bdb38f3c9a2c82a12a0b09138e9a8b4f14fe66
Richard Lander <lander2k2@protonmail.com>
2017-11-03 09:44:36 -0400

    Fix vmware datacenter id

--- platforms/vmware/provider.tf
+++ platforms/vmware/provider.tf
@@ -4,8 +4,12 @@ provider "vsphere" {
   allow_unverified_ssl = "${var.tectonic_vmware_sslselfsigned}"
 }
 
+data "vsphere_datacenter" "dc" {
+  name = "${var.tectonic_vmware_worker_datacenters[0]}"
+}
+
 resource "vsphere_folder" "tectonic_vsphere_folder" {
   path          = "${var.tectonic_vmware_folder}"
   type          = "${var.tectonic_vmware_type}"
-  datacenter_id = "${var.tectonic_vmware_worker_datacenters[0]}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
