1730285ad4c34797f37f9d085b2e18fff7784a64
core <core@jumpbox.scottsumner.net>
2017-12-14 13:23:03 -0500

    Revert vsphere provider version

--- platforms/vmware/provider.tf
+++ platforms/vmware/provider.tf
@@ -1,15 +1,10 @@
 provider "vsphere" {
-  version              = "0.4.2"
+  version              = "0.2.2"
   vsphere_server       = "${var.tectonic_vmware_server}"
   allow_unverified_ssl = "${var.tectonic_vmware_sslselfsigned}"
 }
 
-data "vsphere_datacenter" "tectonic_dc" {
-  name = "${var.tectonic_vmware_worker_datacenters[0]}"
-}
-
 resource "vsphere_folder" "tectonic_vsphere_folder" {
   path          = "${var.tectonic_vmware_folder}"
-  type          = "${var.tectonic_vmware_type}"
-  datacenter_id = "${data.vsphere_datacenter.tectonic_dc.id}"
+  datacenter    = "${var.tectonic_vmware_worker_datacenters[0]}"
 }
